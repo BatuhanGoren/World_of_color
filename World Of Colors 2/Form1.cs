e49ce9115b64c55028532c537572857d7d6522a9
Batuhan Goren <batuhangrn@outlook.com>
2021-01-16 20:39:31 +0300

    World Of Color

---
+++ World Of Colors 2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace World_Of_Colors_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                Form3 woc = new Form3();
+                woc.Show();
+                this.Hide();
+                MessageBox.Show("Welcome :)");
+          
+             }
+            else
+            {
+                MessageBox.Show("Your username or password is incorrect");
+                textBox1.Clear();
+                textBox2.Clear(); 
+            }
+
+            
+
+        }
+    }
+}
---
+++ World Of Colors 2/Form3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace World_Of_Colors_2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text=="Blue")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text +" "+"is" +" "+"Symbolizes cuteness and harmony, cheers and relaxes people.";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.Blue;
+                
+
+            }
+            if (comboBox1.Text == "Black")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text + " " + "is" + " " + " Symbolizes strength, self-confidence, discipline and might.";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.Black;
+            }
+            if (comboBox1.Text=="White")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text + " " + "is" + " " + " Represents cold-bloodedness, nobility, innocence, stability and continuity. It gives peace and confidence.";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.White;
+            }
+            if (comboBox1.Text=="Green")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text + " " + "is" + " " + " Represents harmony and balance.";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.Green;
+            }
+            if (comboBox1.Text=="Red")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text + " " + "is" + " " + " Represents happiness and stimulates appetite.";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.Red;
+
+            }
+            if (comboBox1.Text=="Yellow")
+            {
+                label3.Text = textBox1.Text + "," + comboBox1.Text + " " + "is" + " " + " Symbolizes existence, life, intelligence, desires and spiritual development. ";
+                button1.BackgroundImage = null;
+                button1.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
